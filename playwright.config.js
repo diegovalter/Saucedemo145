6a8196ddc92fbdedbc06dd608487b1bd9f5de099
diegovalter <di_valter@hotmail.com>
2025-10-01 22:16:53 -0300

    diretorios

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,44 @@
-const { defineConfig } = require('@playwright/test')
+const { defineConfig, devices } = require('@playwright/test')
+const path = require('path')
+const { compute_run_folder, ensure_subdirs } = require('./utils/path_tools')
+
+// Diretórios onde ficam os artefatos
+const ARTIFACTS_ROOT = path.join(__dirname, 'artifacts') // nome pasta raiz
+const runDir = compute_run_folder(ARTIFACTS_ROOT)
+const { resultsDir, screenshotsDir } = ensure_subdirs(runDir)
+
+// Expôe caminhos de diretórios como variáveis de ambiente
+process.env.RUN_DIR         = runDir
+process.env.SCREENSHOTS_DIR = screenshotsDir
 
 module.exports = defineConfig({
+    testDir: '.tests',      // nossos testes estão na pasta tests
+    timeout: 30000,         // 30_000 = 30 segundos
+    fullyParallel: true,    // execução em paralelo / multi thread
+    outputDir: resultsDir,  
     use: {
         baseURL: 'https://www.saucedemo.com',
         headless: false,    // false - exibe o browser e true oculta
+        // Politicas globais de artefatos automaticos
+        screenshot: 'only-on-failure',      // apenas quando der erro
+        video:      'retain-on-failure',    // salva o vídeo, se houver erro
+        trace:      'retain-on-failure',    // salva o trace, se houver erro
+
+        // outros tipos de timeout
+        actionTimeout: 15000, // timeout se nada estiver acontecendo em 15s
+        navigationTimeout: 20000, // timeout se parar o navegador
+
         launchOptions: {
-            slowMo: 1000    // espere 1 segundo entre cada ação
+            slowMo: 0    // espere 1 segundo entre cada ação
         }
-    }
+    },
+
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome']}
+        }
+    ]
+
+
 })
