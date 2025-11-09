23aceb19d7463942eb0345f40634df6ef59886bc
diegovalter <di_valter@hotmail.com>
2025-11-08 22:50:56 -0300

    teste1finalizado

--- tests/comprar_mochila_strut.spec.js
+++ tests/comprar_mochila_strut.spec.js
@@ -35,6 +35,34 @@ async function add_to_cart_step(page){
             await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText("$29.99")
 }
 
+async function fill_checkout_step(page){
+    await page.locator('[data-test="checkout"]').click()
+    await expect(page).toHaveURL(/checkout-step-one\.html/)
+    await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Your Information')
+    await page.locator('[data-test="firstName"]').fill('NomeTeste')
+    await page.locator('[data-test="lastName"]').fill('SobrenomeTeste')
+    await page.locator('[data-test="postalCode"]').fill('01023-003')
+
+}
+
+async function checkout_confirm(page){
+    await page.locator('[data-test="continue"]').click()
+    await expect(page).toHaveURL(/checkout-step-two\.html/)
+    await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Overview')
+    await expect(page.locator('[data-test="item-quantity"]')).toHaveText('1')
+    await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText('Sauce Labs Backpack')
+    await expect(page.locator('[data-test="payment-info-value"]')).toHaveText('SauceCard #31337')
+    await expect(page.locator('[data-test="total-label"]')).toHaveText('Total: $32.39')
+}
+
+async function finish_step(page){
+    await page.locator('[data-test="finish"]').click()
+    await expect(page).toHaveURL(/checkout-complete\.html/)
+    await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Complete!')
+    await expect(page.locator('[data-test="complete-header"]')).toHaveText('Thank you for your order!')
+}
+    
+
 test.describe('SauceDemo - fluxo principal de compra', () => {
     test('Comprar Mochila Direto', 
         async({ page }, testInfo) => {
@@ -63,62 +91,82 @@ test.describe('SauceDemo - fluxo principal de compra', () => {
             await expect(page.locator('.shopping_cart_badge')).toHaveText('1')
             await snap(page, testInfo, 'TC001-Passo03-Mochila-Adicionada')
         }) // fim do passo 3
-
+        
+        // Inicio do passo 4
         await test.step('Ir para o carrinho', async () => {
             await add_to_cart_step(page)
             await snap(page, testInfo, 'TC001-Passo04-Carrinho-Conferido')
-        })  
+        })  // fim do passo 4
+
+        // Inicio do Passo 5
+        await test.step('Ir para o Checktout', async () => {
+            await fill_checkout_step(page)
+            await snap(page, testInfo, 'TC001-Passo05-Checkout-Preenchido')
+        }) // fim do passo 5
+
+        // Inicio do Passo 6
+        await test.step('Ir para Finalizacao do Checkout', async () => {
+            await checkout_confirm(page)
+            await snap(page, testInfo, 'TC001-Passo06-Finalizao-Checkout')    
+        }) // fim do passo 6
+
+                // Inicio do Passo 7
+        await test.step('Ir para Confirmacao da Compra', async () => {
+            await finish_step(page)
+            await snap(page, testInfo, 'TC001-Passo07-Confirmacao-Compra')    
+        }) // fim do passo 7
 
     }) // fim do test 1
-     test('Comprar Mochila Detalhe' , 
-        async({ page }, testInfo) => {
-        testInfo.setTimeout(testInfo.timeout + 15000)
+    
+    //  test('Comprar Mochila Detalhe' , 
+    //     async({ page }, testInfo) => {
+    //     testInfo.setTimeout(testInfo.timeout + 15000)
 
-         // Inicio do Passo 1
-        await test.step('Acessar SauceDemo.com', async () => {
-            await login_step(page) 
-            await snap(page, testInfo, 'TC002-Passo01-Home')           
-        }) // fim do passo 1
+    //      // Inicio do Passo 1
+    //     await test.step('Acessar SauceDemo.com', async () => {
+    //         await login_step(page) 
+    //         await snap(page, testInfo, 'TC002-Passo01-Home')           
+    //     }) // fim do passo 1
  
-        // Inicio do passo 2
-        await test.step('Login com Sucesso', async () => {
-           await success_login_fill_step(page)
-           await snap(page, testInfo, 'TC002-Passo02A-Login_Preenchido')
-           await success_login_click_step(page)
-           await snap(page, testInfo, 'TC002-Passo02B-Inventory')
-        }) // fim do passo 2
+    //     // Inicio do passo 2
+    //     await test.step('Login com Sucesso', async () => {
+    //        await success_login_fill_step(page)
+    //        await snap(page, testInfo, 'TC002-Passo02A-Login_Preenchido')
+    //        await success_login_click_step(page)
+    //        await snap(page, testInfo, 'TC002-Passo02B-Inventory')
+    //     }) // fim do passo 2
 
-        // Inicio do passo 3
-        await test.step('Abrir pagina da mochila e dicionar no carrinho', async () => {
-            // Parte 3.1 - Abrir a parina da mochila
-            // Ação
-            //const seletor_mochila = page.locator('.inventory_item').filter({ hasText: /Backpack/ })
-            //await seletor_mochila.getByRole('button', { name: /Add to cart/ }).click()
-            await page.locator('[data-test="item-4-title-link"]').click()
-            // Verificações
-            await expect(page).toHaveURL(/inventory-item\.html/)   // url
-            await expect(page).toHaveTitle('Swag Labs')             // title (guia)
-            await expect(page.locator('[data-test="back-to-products"]')).toHaveText('Back to products')
+    //     // Inicio do passo 3
+    //     await test.step('Abrir pagina da mochila e dicionar no carrinho', async () => {
+    //         // Parte 3.1 - Abrir a parina da mochila
+    //         // Ação
+    //         //const seletor_mochila = page.locator('.inventory_item').filter({ hasText: /Backpack/ })
+    //         //await seletor_mochila.getByRole('button', { name: /Add to cart/ }).click()
+    //         await page.locator('[data-test="item-4-title-link"]').click()
+    //         // Verificações
+    //         await expect(page).toHaveURL(/inventory-item\.html/)   // url
+    //         await expect(page).toHaveTitle('Swag Labs')             // title (guia)
+    //         await expect(page.locator('[data-test="back-to-products"]')).toHaveText('Back to products')
 
-            await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText("Sauce Labs Backpack")
-            await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText("$29.99")
+    //         await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText("Sauce Labs Backpack")
+    //         await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText("$29.99")
 
             
-            await snap(page, testInfo, 'TC002-Passo03_1-Inventory_Item')
+    //         await snap(page, testInfo, 'TC002-Passo03_1-Inventory_Item')
 
-            // Parte 3.2 - Adcionar produto no carrinho
-            // Ação
-            await page.locator('[data-test="add-to-cart"]').click()
-            // Verificações          
-            await expect(page.locator('.shopping_cart_badge')).toHaveText('1')
-            await snap(page, testInfo, 'TC001-Passo03_2-Mochila-Adicionada')
-        }) // fim do passo 3
+    //         // Parte 3.2 - Adcionar produto no carrinho
+    //         // Ação
+    //         await page.locator('[data-test="add-to-cart"]').click()
+    //         // Verificações          
+    //         await expect(page.locator('.shopping_cart_badge')).toHaveText('1')
+    //         await snap(page, testInfo, 'TC001-Passo03_2-Mochila-Adicionada')
+    //     }) // fim do passo 3
 
-        await test.step('Ir para o carrinho', async () => {
-            await add_to_cart_step(page)
-            await snap(page, testInfo, 'TC002-Passo04-Carrinho-Conferido')
-        })
+    //     await test.step('Ir para o carrinho', async () => {
+    //         await add_to_cart_step(page)
+    //         await snap(page, testInfo, 'TC002-Passo04-Carrinho-Conferido')
+    //     })
        
  
-    }) // fim do test 2
+    // }) // fim do test 2
 }) // fim do describe
